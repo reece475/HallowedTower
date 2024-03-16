3ae9d0563fdb790d7e071ad0e50f5d45d22bea9e
reece475 <reeceleonetti2@gmail.com>
2024-03-16 17:44:03 -0400

    Quick Fixes

--- js/base.js
+++ js/base.js
@@ -1,9 +1,3 @@
-//When the body tag is loaded, bottom of page will display when the page was last updated
-function lastUpdated() {
-    var x = document.lastModified;
-    document.getElementById("lastUpdated").innerHTML = "This page was last modified " + x;
-  }
-
   //Long page, so this lets you jump back to the top
 function backToTop(){
     document.body.scrollTop = 0; // For Safari
