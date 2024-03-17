7e9bafe5c0b0766e4e74ff5a36dd8e72c446c38a
reece475 <reeceleonetti2@gmail.com>
2024-03-16 22:38:13 -0400

    Mobile View bugfix

--- js/base.js
+++ js/base.js
@@ -3,3 +3,14 @@ function backToTop(){
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera    
 }
+
+// Allows Nav and Aside to be toggled in Mobile View
+function hideMenu(){
+  if (document.getElementById("navSidebar").style.display === "block"){
+      document.getElementById("navSidebar").style.display = "none";
+      document.getElementById("asideSidebar").style.display = "none";
+  }else{
+    document.getElementById("navSidebar").style.display = "block";
+    document.getElementById("asideSidebar").style.display = "block";
+  }
+}
