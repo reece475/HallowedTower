02e214b8ce4aaf30141500351b14817f1153277a
reece475 <reeceleonetti2@gmail.com>
2024-03-16 23:01:17 -0400

    .load Stuff

--- js/load.js
+++ js/load.js
@@ -1,3 +1,6 @@
+//When the page is fully loaded, this function will load the contents of the txt files into the assigned IDs
 $(function(){
-    $("#navSidebar").load("../navSidebar.txt");
+    $("#navSidebar").load("../sidebars/navSidebar.txt");
+    $("#asideSidebar").load("../sidebars/asideSidebar.txt");
+    $("#footerSidebar").load("../sidebars/footerInfo.txt");
   });
