a580858c756c1c790c462cbeacd711830ae7411c
reece475 <reeceleonetti2@gmail.com>
2024-04-16 09:29:12 -0400

    Load File Fixes

--- js/load.js
+++ js/load.js
@@ -1,6 +1,6 @@
 //When the page is fully loaded, this function will load the contents of the txt files into the assigned IDs
 $(function(){
-    $("#navSidebar").load("../sidebars/navSidebar.txt");
-    $("#asideSidebar").load("../sidebars/asideSidebar.txt");
-    $("#footerInfo").load("../sidebars/footerInfo.txt");
+    $("#navSidebar").load("../../sidebars/navSidebar.txt");
+    $("#asideSidebar").load("../../sidebars/asideSidebar.txt");
+    $("#footerInfo").load("../../sidebars/footerInfo.txt");
   });
