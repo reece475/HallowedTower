b66873a1a208d81897972f9185725789e7406abd
reece475 <reeceleonetti2@gmail.com>
2024-08-19 20:13:05 -0400

    Create johtoSafariZone.js

---
+++ js/johtoSafariZone.js
@@ -0,0 +1,14 @@
+//Used to generate the encounters from the safari zone
+
+class safari {
+    constructor(pokemon, sprite, biome, location, block1, block2, level, time){
+        this.pokemon = pokemon;
+        this.sprite = sprite;
+        this.biome = biome;
+        this.location = location;
+        this.block1 = block1;
+        this.block2 = block2;
+        this.level = level;
+        this.time = time;
+    }
+}
